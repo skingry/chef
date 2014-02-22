6b8ed3503ad76e119bb9de62fe6bd8c723a81a97
Seth Kingry <sjkingry@gmail.com>
2014-02-22 18:11:28 -0500

    Initial commit of the sabnzbd cookbook

---
+++ cookbooks/sabnzbd/metadata.rb
@@ -0,0 +1,13 @@
+maintainer       "Seth Kingry"
+maintainer_email "sjkingry@gmail.com"
+license          "Apache v2"
+description      "Installs and Configures SABnzbd"
+version          "0.1.0"
+
+depends          "python-cheetah"
+depends          "python-yenc"
+depends          "unzip"
+depends          "unrar"
+depends          "par2"
+depends          "nginx"
+
