272ac2c68e8e8eee6fff824e99e4f205a9eeb560
Seth Kingry <sjkingry@gmail.com>
2014-03-04 19:12:26 -0500

    Changes to the cache and cookbooks dir of the knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,10 +1,15 @@
-@chefdir    = File.expand_path("#{File.dirname(__FILE__)}")
+current_dir             = File.dirname(__FILE__)
 
 log_level                 :info
 log_location              STDOUT
 node_name                 "skingry"
 chef_server_url           "https://chef.local.pvt"
 
+cache_type                "BasicFile"
+cache_options({ :path => "#{current_dir}/cache/checksums", :skip_expires => true })
+
 validation_client_name    "chef-validator"
-validation_key            "#{@chefdir}/secrets/chef-validator.pem"
-client_key                "#{@chefdir}/secrets/#{node_name}.pem"
+validation_key            "#{current_dir}/secrets/chef-validator.pem"
+client_key                "#{current_dir}/secrets/#{node_name}.pem"
+
+cookbook_path             "#{current_dir}/../cookbooks"
