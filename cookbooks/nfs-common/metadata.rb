f149ac99b058e7ad090824229b49b53004ca6a49
Seth Kingry <sjkingry@gmail.com>
2014-03-04 19:16:14 -0500

    Removing all the cookbooks and updating the gitignore file

--- cookbooks/nfs-common/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Seth Kingry"
-maintainer_email "sjkingry@gmail.com"
-license          "Apache v2"
-description      "Installs NFS Common Files"
-version          "0.1.0"
-
-supports         "ubuntu", ">= 12.04"
