cc983885e614b4102b8ef11bae27b3e302ba8a68
Seth Kingry <sjkingry@gmail.com>
2014-10-29 22:53:39 -0400

    Initial work on Musicbrainz

---
+++ site-cookbooks/musicbrainz/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Seth Kingry"
+maintainer_email "sjkingry@gmail.com"
+license          "Apache v2"
+description      "Installs and Configures Musicbrainz"
+version          "0.1.0"
+
+depends          "apt"
+depends          "perl"
+
