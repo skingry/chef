09b1d38064c60712f41a8f0ced3da7c28cac8420
Seth Kingry <sjkingry@gmail.com>
2016-04-08 15:00:44 -0400

    cleanup

--- solo.rb
+++ solo.rb
@@ -1,18 +1,18 @@
-node_name "monolith.robotozon.com"
+node_name                  "monolith.robotozon.com"
 
-file_cache_path    "/var/chef/cache"
-file_backup_path   "/var/chef/backup"
+file_cache_path            "/var/chef/cache"
+file_backup_path           "/var/chef/backup"
 
-log_level :info
-verbose_logging false
+log_level                  :info
+verbose_logging            false
 
-encrypted_data_bag_secret  "/home/skingry/chef/.chef/secrets/encrypted_data_bag_secret"
+encrypted_data_bag_secret  "/home/skingry/chef/encrypted_data_bag_secret"
 
-cookbook_path [ "/home/skingry/chef/cookbooks", "/home/skingry/chef/site-cookbooks" ]
-environment_path "/home/skingry/chef/environments"
-role_path "/home/skingry/chef/roles"
-data_bag_path "/home/skingry/chef/data_bags"
+cookbook_path              "/home/skingry/chef/cookbooks"
+environment_path           "/home/skingry/chef/environments"
+role_path                  "/home/skingry/chef/roles"
+data_bag_path              "/home/skingry/chef/data_bags"
 
-environment "production"
+environment                "production"
 
-json_attribs "/home/skingry/chef/config.json"
+json_attribs               "/home/skingry/chef/config.json"
