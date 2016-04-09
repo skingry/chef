b59baa8f80b849e6a227f76a933660a23db55367
Seth Kingry <sjkingry@gmail.com>
2016-04-09 10:51:56 -0400

    hulk squash

--- solo.rb
+++ solo.rb
@@ -6,13 +6,9 @@ file_backup_path           "/var/chef/backup"
 log_level                  :info
 verbose_logging            false
 
-encrypted_data_bag_secret  "/home/skingry/chef/encrypted_data_bag_secret"
-
 cookbook_path              [ "/home/skingry/chef/cookbooks", "/home/skingry/chef/site-cookbooks" ]
 environment_path           "/home/skingry/chef/environments"
 role_path                  "/home/skingry/chef/roles"
 data_bag_path              "/home/skingry/chef/data_bags"
-
-environment                "production"
 
 json_attribs               "/home/skingry/chef/config.json"
