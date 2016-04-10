a16e29d2f25e692b77a93a418429071df7753b3e
Seth Kingry <sjkingry@gmail.com>
2016-04-09 20:44:14 -0400

    updates to the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,7 @@
 apt-add-repository --yes ppa:zfs-native/stable
 apt-get update
 apt-get -y install ubuntu-zfs
-
+modprobe zfs
+zpool create -f tank raidz /dev/sdb /dev/sdc /dev/sdd /dev/sde
+zfs create -o mountpoint=/data tank/data
+curl -L https://www.opscode.com/chef/install.sh | sudo bash
