4dcdc41c91493cd7620eb1c8f193ac3ec22feed2
Seth Kingry <sjkingry@gmail.com>
2016-09-27 12:20:18 -0400

    update the Vagrant local file

--- Vagrantfile.local.rb
+++ Vagrantfile.local.rb
@@ -1,12 +1,14 @@
+domain = "media-server.local"
+
 $vagrant[:name] = "Media Server"
 $vagrant[:ssh_username] = "vagrant"
-$vagrant[:hostname] = "monolith.robotozon.com"
-$vagrant[:hostname_aliases] = [ "couchpotato.robotozon.com",
-                                "plex.robotozon.com",
-                                "plexpy.robotozon.com",
-                                "sabnzbd.robotozon.com",
-                                "sonarr.robotozon.com",
-                                "transmission.robotozon.com" ]
+$vagrant[:hostname] = "monolith.#{domain}"
+$vagrant[:hostname_aliases] = [ "couchpotato.#{domain}",
+                                "plex.#{domain}",
+                                "plexpy.#{domain}",
+                                "sabnzbd.#{domain}",
+                                "sonarr.#{domain}",
+                                "transmission.#{domain}" ]
 $vagrant[:main_ip] = "192.168.56.101"
 $vagrant[:cpus] = 4
 $vagrant[:memory_size] = 4096
