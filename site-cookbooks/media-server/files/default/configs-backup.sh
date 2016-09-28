dd74de4dca843f41aaef5c27f6460f67e8613e03
Seth Kingry <sjkingry@gmail.com>
2016-09-28 11:21:20 -0400

    update backup script and proxy configs

--- site-cookbooks/media-server/files/default/configs-backup.sh
+++ site-cookbooks/media-server/files/default/configs-backup.sh
@@ -3,12 +3,17 @@
 # Record current date
 DATE=`date +%Y%m%d`
 
+# Get the hostname of the server
+HOSTNAME=`hostname -f`
+
+BACKUP_FILE="${HOSTNAME}configs-backup-${DATE}.tgz"
+
 # Create backup file
-tar -cf - /data/configs | gzip -c > /tmp/configs-backup-${DATE}.tgz
+tar -cf - /data/configs | gzip -c > /tmp/${BACKUP_FILE}
 
 # Send the backup file to S3
-aws s3 cp /tmp/configs-backup-${DATE}.tgz s3://s3.robotozon.com/backups/
+aws s3 cp /tmp/${BACKUP_FILE} s3://s3.robotozon.com/backups/
 
 # Clean up after ourselves
-rm /tmp/configs-backup-${DATE}.tgz
+rm /tmp/${BACKUP_FILE}
 
