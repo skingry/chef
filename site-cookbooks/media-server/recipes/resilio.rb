4e329fcdd1010dc9b294c4fd7186ff640b556501
Seth Kingry <sjkingry@gmail.com>
2017-11-27 09:07:56 -0500

    Adding Resilio

---
+++ site-cookbooks/media-server/recipes/resilio.rb
@@ -0,0 +1,54 @@
+#
+# Cookbook Name:: media-server
+# Recipe:: resilio
+#
+# Copyright 2014, Seth Kingry
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+domain = node[:media_server][:domain]
+host = node[:media_server][:host][:resilio]
+name = 'resilio'
+port = '8888'
+repo = "skingry/#{name}"
+
+include_recipe 'directories'
+
+directory "/data/configs/#{name}" do
+  owner 'nobody'
+  group 'nogroup'
+end
+
+docker_image "#{name}" do
+  repo "#{repo}"
+  action :pull
+  notifies :redeploy, "docker_container[#{name}]"
+end
+
+docker_container "#{name}" do
+  repo "#{repo}"
+  network_mode 'host'
+  volumes [ '/data:/data', '/dev/rtc:/dev/rtc:ro', '/etc/localtime:/etc/localtime:ro', '/data/configs/resilio:/.sync' ]
+  restart_policy 'always'
+end
+
+template "/data/configs/nginx/sites/#{name}.conf" do
+  source 'proxy_site.erb'
+  variables :domain => "#{domain}",
+            :host => "#{host}",
+            :name => "#{name}",
+            :port => "#{port}",
+            :auth => false
+end
+
