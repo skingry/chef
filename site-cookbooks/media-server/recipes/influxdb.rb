b860052dc3af25b18a99feb009d21dd6f642927a
Seth Kingry <sjkingry@gmail.com>
2018-01-28 20:56:04 -0500

    Removing swap requirement from InfluxDb

--- site-cookbooks/media-server/recipes/influxdb.rb
+++ site-cookbooks/media-server/recipes/influxdb.rb
@@ -32,7 +32,6 @@ end
 docker_container "#{name}" do
   repo "#{repo}"
   memory '1073741824'
-  memory_swap '2147483648'
   port '127.0.0.1:8086:8086'
   volumes [ '/data/configs/influxdb:/var/lib/influxdb', '/data/configs/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro' ]
   restart_policy 'always'
