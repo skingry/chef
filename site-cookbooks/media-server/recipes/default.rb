fb2ba974e23052bb73fc96c6d89e65f975f883da
Seth Kingry <sjkingry@gmail.com>
2018-01-30 15:48:47 -0500

    Adding monitoring

--- site-cookbooks/media-server/recipes/default.rb
+++ site-cookbooks/media-server/recipes/default.rb
@@ -26,6 +26,8 @@ end
 
 include_recipe 'directories'
 
+include_recipe 'media-server::monitoring'
+
 include_recipe 'media-server::openvpn'
 include_recipe 'media-server::backup'
 include_recipe 'media-server::certbot'
