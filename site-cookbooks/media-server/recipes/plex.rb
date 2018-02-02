cf7252bb53f6a1005f1ca3aaa60ec5b76880c6d5
Seth Kingry <sjkingry@gmail.com>
2018-02-02 00:27:39 -0500

    Update Plex

--- site-cookbooks/media-server/recipes/plex.rb
+++ site-cookbooks/media-server/recipes/plex.rb
@@ -32,7 +32,7 @@ docker_container "#{name}" do
   memory '2048M'
   network_mode 'host'
   runtime 'nvidia'
-  volumes [ '/data/configs/plex:/config', '/data/shares:/shares', '/usr/lib/nvidia-384:/usr/lib/nvidia-384:ro', '/dev/rtc:/dev/rtc:ro', '/etc/localtime:/etc/localtime:ro', '/tmp:/tmp' ]
+  volumes [ '/data/configs/plex:/config', '/data/shares/Media:/media', '/usr/lib/nvidia-384:/usr/lib/nvidia-384:ro', '/dev/rtc:/dev/rtc:ro', '/etc/localtime:/etc/localtime:ro', '/tmp:/tmp' ]
   privileged true
   restart_policy 'always'
 end
