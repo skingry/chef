8bd0a2804dc7be2e0313a52e5b345362584ff6ac
Seth Kingry <sjkingry@gmail.com>
2018-02-28 15:00:28 -0500

    Update transmission.rb

--- site-cookbooks/media-server/recipes/transmission.rb
+++ site-cookbooks/media-server/recipes/transmission.rb
@@ -17,30 +17,20 @@
 # limitations under the License.
 #
 
-domain = node[:media_server][:domain]
-name = 'transmission'
-host = "#{name}"
-port = '9091'
-
-docker_image "#{name}" do
-  source "/root/Dockerfiles/#{name}"
+docker_image 'transmission' do
+  source '/root/Dockerfiles/transmission'
   action :build_if_missing
 end
 
-docker_container "#{name}" do
-  repo "#{name}"
+docker_container 'transmission' do
+  repo 'transmission'
   memory '128M'
   network_mode 'container:openvpn'
-  volumes [ '/data/configs/transmission:/config', '/data/shares/Downloads:/download', '/dev/rtc:/dev/rtc:ro', '/etc/localtime:/etc/localtime:ro' ]
+  volumes [ 
+            '/data/configs/transmission:/config', 
+            '/data/shares/Downloads:/download', 
+            '/dev/rtc:/dev/rtc:ro', 
+            '/etc/localtime:/etc/localtime:ro' 
+          ]
   restart_policy 'always'
 end
-
-template "/data/configs/nginx/sites/#{name}.conf" do
-  source 'proxy_site.erb'
-  variables :domain => "#{domain}",
-            :host => "#{host}",
-            :name => "#{name}",
-            :port => "#{port}",
-            :auth => true
-end
-
